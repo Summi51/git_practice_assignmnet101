4cc12f842978196d69473dfd8001e49a1608c94f
conan-exploits <inayatsamreen1411@gmail.com>
2022-09-21 03:31:54 -0700

    code fixed by Samreen

--- prime.js
+++ prime.js
@@ -1,9 +1,22 @@
 function prime(num){
-for(let i=1; i<=num; i++){
-if(num%i){
-return true;
+let factor = 0;
+for(let i=1; i<=num;i++){
+if(num%i==0){
+factor++;
+}
+}
+if(factor==2){
+return tue;
 }
 return false;
 }
+let answer =prime(19);
+if(answer ==true){
+console.log("Prime);
 }
-prime(19);
+else{
+console.log("Not Prime");
+}
+
+
+
